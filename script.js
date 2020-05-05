bd56ea00c8bf6d8040f8a46b39d7016e2365947e
h0verr <64825447+h0verr@users.noreply.github.com>
2020-05-05 22:21:04 +0600

    Update style.css

--- script.js
+++ script.js
@@ -22,41 +22,54 @@
 })();
 function myFunction() {
     var x;
-    x = document.getElementById('email').value;
+    x = document.getElementById('item').value;
     let z = /([a-zA-Z0-9]+)([\.{1}])?([a-zA-Z0-9]+)\@gmail([\.])com$/g.test(x);
-    try {
-        if (!z) throw "please enter to the gmail post";
-    }
-    catch (err) {
-        alert("Input " + err);
+    if (!z) {
+        alert(" please enter to the gmail post");
+        return false;
     }
     let pswr = document.querySelector('#password').value;
     let test = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g.test(pswr);
-    try {
-        if (!test) throw "please enter to the right password";
-    }
-    catch (err) {
-        alert("Input " + err);
+    if (!test) {
+        alert("please enter to the gmail post");
+        return false;
     }
     let phone = document.querySelector('#phone').value;
     let test2 = /(\+7|8)[- _]*\(?[- _]*(\d{3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})/g.test(phone);
-    try {
-        if (!test2) throw "please enter to the right number";
-    }
-    catch (err) {
-        alert("Input " + err);
+    if (!test2) {
+        alert("please enter to the gmail post");
+        return false;
     }
 }
 function dtr() {
     const input = document.getElementById('item');
     let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
     localStorage.setItem('items', JSON.stringify(itemsArray));
-    function myFunction() {
+    function myFunction1() {
         itemsArray.push(input.value);
         localStorage.setItem('items', JSON.stringify(itemsArray));
         input.value = ''
     }
+    const input1 = document.getElementById('password');
+    let itemsArray1 = localStorage.getItem('items1') ? JSON.parse(localStorage.getItem('items1')) : [];
+    localStorage.setItem('items1', JSON.stringify(itemsArray1));
+    function myFunction2() {
+        itemsArray1.push(input1.value);
+        localStorage.setItem('items1', JSON.stringify(itemsArray1));
+        input1.value = ''
+    }
+    const input2 = document.getElementById('phone');
+    let itemsArray2 = localStorage.getItem('items2') ? JSON.parse(localStorage.getItem('items2')) : [];
+    localStorage.setItem('items2', JSON.stringify(itemsArray2));
+    function myFunction3() {
+        itemsArray2.push(input2.value);
+        localStorage.setItem('items2', JSON.stringify(itemsArray2));
+        input2.value = ''
+    }
     myFunction();
+    myFunction1();
+    myFunction2();
+    myFunction3();
 }
 /*function emailValidation() {
     var email = document.querySelectorAll('input[data-rule]');
